3e096328b1d557dcad177b09b8371d312d97a392
Diego Volpatto <volpatto@lncc.br>
2020-11-15 04:31:27 -0300

    Create cpp interface for runge_kutta4 integrate_const

---
+++ examples/boost/call_sodes_rk4.cpp
@@ -0,0 +1,46 @@
+#include "BoostOdeint.hpp"
+
+#include <Eigen/Core>
+
+#include <tuple>
+#include <iostream>
+
+using namespace sodes::odeint;
+using namespace Eigen;
+
+// System to be solved
+void lorenz(const ArrayXd &x, ArrayXd &dxdt, const double &t)
+{
+    double sigma = 10.0;
+    double R = 28.0;
+    double b = 8.0 / 3.0;
+
+    dxdt[0] = sigma * (x[1] - x[0]);
+    dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
+    dxdt[2] = -b * x[2] + x[0] * x[1];
+}
+
+int main (){
+    // Defining the state vector with initial conditions
+    int num_of_states = 3;
+    ArrayXd X(num_of_states);
+    X << 0., 1., 0.1;
+
+    // Set time range and increment
+    auto t_init = 0.0;
+    auto t_final = 10.0;
+    auto t_span = std::make_tuple(t_init, t_final);
+    auto dt = 0.01;
+
+    // Containers to store solution
+    std::vector<ArrayXd> x_sol;
+    std::vector<double> times;
+    std::tie(times, x_sol) = solve_ivp(lorenz, t_span, dt, X);
+
+    // Displaying result on terminal
+    auto steps = times.size();
+    for (size_t i = 0; i < steps; i++)
+    {
+        std::cout << times[i] << '\t' << x_sol[i][0] << '\t' << x_sol[i][1] << '\t' << x_sol[i][2] << '\n';
+    }
+}
---
+++ pysodes/sodes/odeint/BoostOdeint.hpp
@@ -0,0 +1,92 @@
+#ifndef SODES_BOOSTODEINT_HPP
+#define SODES_BOOSTODEINT_HPP
+
+#include <Eigen/Core>
+#include <boost/numeric/odeint.hpp>
+#include <boost/numeric/odeint/algebra/vector_space_algebra.hpp>
+#include <boost/numeric/odeint/external/eigen/eigen_resize.hpp>
+
+#include <tuple>
+#include <functional>
+#include <vector>
+#include <utility>
+#include <stdexcept>
+
+namespace sodes::detail::odeint {
+
+using namespace boost::numeric::odeint;
+using namespace Eigen;
+using namespace std;
+
+typedef ArrayXd state_type;
+
+struct Observer
+{
+    vector<state_type>& m_states;
+    vector<double>& m_times;
+
+    Observer(vector<state_type>& states, vector<double>& times)
+        : m_states(states), m_times(times) {}
+
+    void operator()(const state_type& x, const double& t)
+    {
+        m_states.push_back(x);
+        m_times.push_back(t);
+    }
+};
+
+pair<vector<double>, vector<state_type>> integrate_rk4(
+    const function<void(const state_type&, state_type&, const double&)>& f,
+    const tuple<double, double>& t_span,
+    const double& dt,
+    state_type& y0)
+{
+    // Containers to store solution
+    vector<state_type> states;
+    vector<double> times;
+
+    // Set custom stepper for Eigen algebra and state
+    typedef runge_kutta4<state_type, double, state_type, double, vector_space_algebra> stepper;
+
+    // Integrate over time
+    auto[t_init, t_final] = t_span;
+    auto observer = Observer(states, times);
+    integrate_const(
+        stepper(),
+        f,
+        y0,
+        t_init,
+        t_final,
+        dt,
+        observer);
+
+    auto solution_pair = make_pair(times, states);
+
+    return solution_pair;
+}
+}  // namespace sodes::detail::odeint
+
+namespace sodes::odeint {
+
+using namespace std;
+using namespace Eigen;
+
+pair<vector<double>, vector<ArrayXd>> solve_ivp(
+    const function<void(const ArrayXd&, ArrayXd&, const double&)>& f,
+    const tuple<double, double>& t_span,
+    const double& dt,
+    ArrayXd& y0,
+    const string& method = "runge_kutta4")
+{
+    using namespace sodes::detail::odeint;
+
+    if (method == "runge_kutta4")
+    {
+        return integrate_rk4(f, t_span, dt, y0);
+    }
+    else
+        throw invalid_argument("Unavailable integration method from Boost::odeint");
+}
+}  // namespace sodes::odeint
+
+#endif //SODES_BOOSTODEINT_HPP
